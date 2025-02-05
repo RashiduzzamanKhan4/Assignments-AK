d7b068ad1e3facdfae52308495d627194a4dec10
Rashhiduzzaman Khan <rashiduzzamankhan2004@gmail.com>
2025-02-04 21:32:26 -0500

    hello_hee

---
+++ testing.java
@@ -0,0 +1,17 @@
+// public class testing {
+    
+// }
+import java.util.Scanner;
+public class testing {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        System.out.println("Hello, it's nice to have you in my Metric Converter!");
+        System.out.println("Please go ahead and enter your query in the format: 17 kh = lb");
+        System.out.println("For now my supported conversions: kg-lb, g-oz, km-mi, mm-in");
+        System.out.println("To quit type 'exit' / '-1'.");
+
+        scanner.close();
+    }
+}
