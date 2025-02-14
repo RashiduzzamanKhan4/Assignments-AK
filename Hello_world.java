3d76b063501f3e5b2ede74d128a818033195e8c0
Rashhiduzzaman Khan <rashiduzzamankhan2004@gmail.com>
2025-02-14 11:57:55 -0500

    Added ReverseString JAR project

---
+++ Hello_world.java
@@ -0,0 +1,32 @@
+public class Hello_world {
+    public static void main(String[] args) {
+        int num1 = 25;
+        int num2 = 15;
+        // age >= 18
+        // age <=40
+        System.out.println(num1 >= 18 && num1 <= 40);
+
+
+
+        // System.out.println(num1 == num2);
+        // System.out.println(num1 != num2);
+        // System.out.println(num1 > num2);
+        // System.out.println(num1 < num2);
+        // System.out.println(num1 >= num2);
+        // System.out.println(num1 <= num2);
+
+        // // num1 = num1 + 5;
+        // // num1 /= 5;
+        // num1++;
+        // System.out.println(num1);
+
+        // int num2 = 7;
+        // System.out.println(num1 + num2);
+        // System.out.println(num1 - num2);
+        // System.out.println(num1 * num2);
+        // System.out.println(num1 / num2);
+        // System.out.println(num2 % num1);
+    }
+
+    
+}
